de2c33d7ef112ea273ece33248abb9598d326586
QuentinBourny <45034206+QuentinBourny@users.noreply.github.com>
2018-11-15 10:33:23 +0100

    Update _main_.py

--- twitterPredictor/tweet_collection/_main_.py
+++ twitterPredictor/tweet_collection/_main_.py
@@ -4,7 +4,10 @@ def Collect(num_candidate):
     Replies=twitter_collect.collect_tweet_candidate_activity.get_replies_to_candidate(num_candidate) #créer une liste des réponses associées au candidat
     Tweets=twitter_collect.collect_candidate_actuality_tweets.get_tweets_from_candidates_search_queries(tweeter_collect.get_candidate_queries(num_candidate ,C:\Users\vince\PycharmProjects\twitterPredictor\CandidateData)) #créer la liste des tweets qui correspondent aux hashtags et mots-clés transmis dans deux fichiers texte par l'équipe du candidat
     #si on souhaite avoir une liste de tout ce qui se rapporte au candidat il suffit de rajouter une liste qui correspond à la concaténation des 3 autres
-#code arrêté ici
 
 def store_tweets(tweets,filename):
-    return None
+    tmp_list=[]
+    for tweet in tweets:
+        tweet_as_dict={"text":tweet.text,"user":tweet.user.id ,"date":tweet.created_at,"hashtags":[hashtag[0]["text"] for hashtag in tweet.entities.hashtags],"retweeted":tweet.retweeted,"retweet_count":tweet.retweet_count}
+        tmp_list.append(tweet_as_dict)
+    json.dump(tmp_list,filename)
