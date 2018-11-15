d71e1b61814535979dc5a908592e0ac99547e206
QuentinBourny <45034206+QuentinBourny@users.noreply.github.com>
2018-11-15 13:02:15 +0100

    Add files via upload

---
+++ twitterPredictor/twitterPredictor/tweet_analysis/Info_Extract.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+"""def higher_retweet():
+    #Find and print the tweet with the higher number of retweets
+    data=
+    rt_max=np.max(data['RTs'])
+    rt=data[data.RTs==rt_max].index[0]
+    #Max RTs:
+    print("The tweet with more retweets is: \n{}".format(data['tweet_textual_content'[rt]]))
+    print("Number of retweets: {}".format(rt_max))
+    print("{} characters.\n".format(data['len'][rt]))
+"""
---
+++ twitterPredictor/twitterPredictor/twitterPredictor/credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'COHRsvSdhEOx6VywsJlM3l1vn'
+CONSUMER_SECRET = 'sVMxesPOBn6v95M407yG79kLhhzfGabQ4fBoC2CIe1VJnrwb1I'
+
+# Access:
+ACCESS_TOKEN  = '1062338951594287106-3ptwNxvF0Nqb9brBkQONY2tttSAtvL'
+ACCESS_SECRET = 'fbtqxKfvGMLNtZqBTr9YB4Ouw0KUCTQzP7MQDMnBtIO4h'
