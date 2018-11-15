04dfd0cc22854464298a4ea7b891d365e4047b8d
QuentinBourny <45034206+QuentinBourny@users.noreply.github.com>
2018-11-15 13:42:14 +0100

    Add files via upload

--- twitterPredictor/tweet_collection/_main_.py
+++ twitterPredictor/tweet_collection/_main_.py
@@ -2,12 +2,26 @@ def Collect(num_candidate):
     connexion=twitter_collect.twitter_connection_setup.twitter_setup()
     Retweets=twitter_collect.collect_tweet_candidate_activity.get_retweets_of_candidate(num_candidate) #créer une liste des retweets associés au candidat
     Replies=twitter_collect.collect_tweet_candidate_activity.get_replies_to_candidate(num_candidate) #créer une liste des réponses associées au candidat
-    Tweets=twitter_collect.collect_candidate_actuality_tweets.get_tweets_from_candidates_search_queries(tweeter_collect.get_candidate_queries(num_candidate ,C:\Users\vince\PycharmProjects\twitterPredictor\CandidateData)) #créer la liste des tweets qui correspondent aux hashtags et mots-clés transmis dans deux fichiers texte par l'équipe du candidat
+    Tweets=twitter_collect.collect_candidate_actuality_tweets.get_tweets_from_candidates_search_queries(tweeter_collect.get_candidate_queries(num_candidate )) #créer la liste des tweets qui correspondent aux hashtags et mots-clés transmis dans deux fichiers texte par l'équipe du candidat
     #si on souhaite avoir une liste de tout ce qui se rapporte au candidat il suffit de rajouter une liste qui correspond à la concaténation des 3 autres
-
+#modifier les noms et prendre aussi les valeurs des likes (voir exemple Fonctionnalité 5)
+import json
 def store_tweets(tweets,filename):
     tmp_list=[]
     for tweet in tweets:
-        tweet_as_dict={"text":tweet.text,"user":tweet.user.id ,"date":str(tweet.created_at),"hashtags":[hashtag[0]["text"] for hashtag in tweet.entities.hashtags],"retweeted":tweet.retweeted,"retweet_count":tweet.retweet_count}
-        tmp_list.append(tweet_as_dict)
+        tweet_as_dict={"text":tweet.text,"user": tweet.user.id ,"date":str(tweet.created_at),"hashtags":tweet.entities.get("hashtags"),"retweeted":tweet.retweeted,"retweet_count":tweet.retweet_count}
+        tmp_list.append(tweet_as_dict)#rajouter condition si liste des hashtags vides ou non
     json.dump(tmp_list,filename)
+    return tmp_list #modification que permet de travailler dans la fonction create_dataframe ci-dessous
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pytest
+
+def create_dataframe(Status):
+    Dataframe=pd.DataFrame(Status)
+    return Dataframe
+import twitter_collect.tweets_collector
+with open('blabla.txt','w') as file:
+    print(create_dataframe(store_tweets(twitter_collect.tweets_collector.collect_by_user("@EmmanuelMacron"),file)))#prendre dans le status(...)
--- twitterPredictor/twitter_collect/collect_tweet_candidate_activity.py
+++ twitterPredictor/twitter_collect/collect_tweet_candidate_activity.py
@@ -1,36 +1,32 @@
-import tweepy
-import twitter_collect
-
 def get_replies_to_candidate(num_candidate):
-   #On veut obtenir toutes les réponses associées à un candidat
-   connexion=twitter_collect.twitter_connection_setup.twitter_setup()
-   replies=[] #liste initialisée qui contiendra les réponses
-   #récupère les messages récents du candidat
+   connexion=twitter_setup()
+   replies=[]
+   #recupere les messages recents du candidat
    statuses = connexion.user_timeline(id = num_candidate, language="fr",rpp=100)
    for full_tweet in statuses:
 
-      #requête pour retrouver des tweets repondant a l'utilisateur num_candidate
+      #query pour retrouver des tweets repondant a l'utilisateur num_candidate
 
        query = 'to:'+ str(num_candidate)
        print (full_tweet.text)
        for tweet in connexion.search(q=query, since_id=992433028155654144, result_type='recent',timeout=999999):
 
-           #si le tweet renvoyé par la requête possède un champs "in reply_to__status_id_str" ie si il s'agit d'une réponse à un tweet
+           #si le tweet renvoye par la requete possede un champs "in reply_to__status_id_str" cest a dire si cest une reponse a un tweet
 
           if hasattr(tweet, 'in_reply_to_status_id_str'):
-               # si c'est une réponse au tweet actuel (full_tweet) du candidat
+               # si c'ets une reponse au tweet actuel (full_tweet) du candidat
               if (tweet.in_reply_to_status_id_str==full_tweet.id_str):
                   replies.append(tweet.text)
                   print(tweet.text)
 
 def get_retweets_of_candidate(num_candidate):
-   connexion=twitter_collect.twitter_connection_setup.twitter_setup()
+   connexion=twitter_setup()
    retweet=[]
-   #récupère les messages récents du candidat
+   #recupere les messages recents du candidat
    statuses = connexion.user_timeline(id = num_candidate, language="fr",rpp=100)
    for full_tweet in statuses:
 
-      #requête pour retrouver des tweets répondant à l'utilisateur num_candidate
+      #query pour retrouver des tweets repondant a l'utilisateur num_candidate
 
        query = 'to:'+str(num_candidate)
        print (full_tweet.text)
